701dae8bf7462cea400365593d673e68665c2b0e
jasperan <23caj23@gmail.com>
2022-11-21 00:43:47 +0100

    feat: improved train.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo apt-get install nvidia-cuda-toolkit unzip -y
+accelerate config
--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 export MODEL_NAME="runwayml/stable-diffusion-v1-5"
 export INSTANCE_DIR="/home/ubuntu/dreambooth_nacho"
-export CLASS_DIR="path_to_class_images"
-export OUTPUT_DIR="path_to_saved_model"
+export CLASS_DIR="/home/ubuntu/class_images"
+export OUTPUT_DIR="/home/ubuntu/trained_model"
 
 accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
@@ -9,8 +9,8 @@ accelerate launch train_dreambooth.py \
   --class_data_dir=$CLASS_DIR \
   --output_dir=$OUTPUT_DIR \
   --with_prior_preservation --prior_loss_weight=1.0 \
-  --instance_prompt="a photo of sks dog" \
-  --class_prompt="a photo of dog" \
+  --instance_prompt="a photo of N" \
+  --class_prompt="a photo of N" \
   --resolution=512 \
   --train_batch_size=1 \
   --sample_batch_size=1 \
